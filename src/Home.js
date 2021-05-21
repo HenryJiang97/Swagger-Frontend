4ff02a2ec18d8c4dfb85f8c5e3a0e2898f8d6e29
Hanhua Jiang <hanhuaj@Hanhuas-MacBook-Pro.local>
2021-05-21 10:22:57 -0700

    Init project

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+    return (
+        <div>
+            <h2>Swagger API Service</h2>
+            <div>
+                <button>Upload</button>
+            </div>
+            <div>
+                <button>Download</button>
+            </div> 
+        </div>
+    );
+}
+
+export default Home;
